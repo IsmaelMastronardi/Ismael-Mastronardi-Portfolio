b2dcf00ed1b1ab808872b14b49b69918aac54f68
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-15 03:44:07 -0300

    carousel changes classes instead of the html

--- src/script/carousel.js
+++ src/script/carousel.js
@@ -1,104 +1,53 @@
-
 import imageUrl from '../assets/images/desktop.png';
 import imageUrl2 from '../assets/images/google.png';
 import imageUrl3 from '../assets/images/vscode.png';
 
-const projects = [
-  {name: 'desktop', img: imageUrl},
-  {name: 'google', img: imageUrl2},
-  {name: 'vscode', img: imageUrl3}
-]
+var projects = document.getElementsByClassName('carouselItem');
 
 let current = 0;
-let prev = current - 1;
-if (current === 0) {
-  prev = projects.length - 1
-}
-let next = current + 1;
-if (current === projects.length) {
-  next = 0
-}
-console.log(projects[prev])
-console.log(projects[current])
-console.log(projects[next])
-
-const caroussel = document.getElementById('caroussel');
-caroussel.innerHTML = `
-  <li class="w-1/3 flex justify-center items-center absolute right-2/4 top-1/2 bottom-1/2">
-    <img src="${projects[prev].img.src}" alt="desktop" class="opacity-80" id="carouselPrev" />
-  </li>
-  <li class="w-1/2 z-10">
-    <img src="${projects[current].img.src}" alt="desktop" class=""/>
-  </li>
-  <li class="w-1/3 flex justify-center items-center absolute left-2/4 top-1/2 bottom-1/2">
-    <img src="${projects[next].img.src}" alt="desktop" class="opacity-80" id="carouselNext"/>
-  </li>
-`;
+let prev = current - 1 < 0 ? projects.length - 1 : current - 1;
+let next = current + 1 >= projects.length ? 0 : current + 1;
 
 const updateCarousel = () => {
-  caroussel.innerHTML = `
-  <li class="w-1/3 flex justify-center items-center absolute right-2/4 top-1/2 bottom-1/2">
-    <img src="${projects[prev].img.src}" alt="desktop" class="opacity-80" id="carouselPrev"/>
-  </li>
-  <li class="w-1/2 z-10">
-    <img src="${projects[current].img.src}" alt="desktop" class=""/>
-  </li>
-  <li class="w-1/3 flex justify-center items-center absolute left-2/4 top-1/2 bottom-1/2">
-    <img src="${projects[next].img.src}" alt="desktop" class="opacity-80" id="carouselNext"/>
-  </li>
-`;
-const prevImg = document.getElementById("carouselPrev");
-prevImg.addEventListener('click', () => carouselBack())
-const nextImg = document.getElementById("carouselNext");
-nextImg.addEventListener('click', () => carouselNext())
-}
+  Array.from(projects).forEach((el, index) => {
+    if (index === current) {
+      el.className = 'w-1/2 z-10 carouselItem';
+      el.lastElementChild.className = '';
+      el.lastElementChild.removeEventListener('click', carouselBack);
+      el.lastElementChild.removeEventListener('click', carouselNext);
+    } else if (index === prev) {
+      el.className = 'w-1/3 flex justify-center items-center absolute right-2/4 top-1/2 bottom-1/2 carouselItem';
+      el.lastElementChild.className = 'opacity-80 carouselBack';
+      el.lastElementChild.removeEventListener('click', carouselNext);
+      el.lastElementChild.addEventListener('click', carouselBack);
+    } else if (index === next) {
+      el.className = 'w-1/3 flex justify-center items-center absolute left-2/4 top-1/2 bottom-1/2 carouselItem';
+      el.firstElementChild.className = 'opacity-80 carouselNext';
+      el.lastElementChild.removeEventListener('click', carouselBack);
+      el.lastElementChild.addEventListener('click', carouselNext);
+    } else {
+      el.className = "w-1/3 flex justify-center items-center absolute left-2/4 top-1/2 bottom-1/2 carouselItem hidden";
+      el.lastElementChild.removeEventListener('click', carouselBack);
+      el.lastElementChild.removeEventListener('click', carouselNext);
+    }
+  });
+};
 
 const carouselBack = () => {
-  if(current === 0){
-    current = projects.length - 1;
-  }
-  else {
-    current = current - 1
-  }
-  prev = current - 1;
-  if (current === 0) {
-    prev = projects.length - 1
-  }
-  next = current + 1;
-  if (current === projects.length - 1) {
-    next = 0
-  }
-  console.log(prev)
-  
-  console.log(current)
-  console.log(next)
+  current = current === 0 ? projects.length - 1 : current - 1;
+  prev = current === 0 ? projects.length - 1 : current - 1;
+  next = current === projects.length - 1 ? 0 : current + 1;
+  updateCarousel();
+};
 
-  updateCarousel()
-}
 const carouselNext = () => {
-  if(current === projects.length - 1){
-    current = 0
-  }
-  else {
-    current = current + 1
-  }
-  prev = current - 1;
-  if (current === 0) {
-    prev = projects.length - 1
-  }
-  next = current + 1;
-  if (current === projects.length - 1) {
-    next = 0
-  }
-  console.log(prev)
-  
-  console.log(current)
-  console.log(next)
+  current = current === projects.length - 1 ? 0 : current + 1;
+  prev = current === 0 ? projects.length - 1 : current - 1;
+  next = current === projects.length - 1 ? 0 : current + 1;
+  updateCarousel();
+};
 
-  updateCarousel()
-}
-
-const prevImg = document.getElementById("carouselPrev");
-prevImg.addEventListener('click', () => carouselBack())
-const nextImg = document.getElementById("carouselNext");
-nextImg.addEventListener('click', () => carouselNext())
+const prevImg = document.getElementsByClassName("carouselBack");
+prevImg[0].addEventListener('click', carouselBack);
+const nextImg = document.getElementsByClassName("carouselNext");
+nextImg[0].addEventListener('click', carouselNext);
