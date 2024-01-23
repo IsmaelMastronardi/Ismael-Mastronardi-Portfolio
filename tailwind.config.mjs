4e0c222529aa37a82296be21a3935a49f328972c
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-23 20:18:51 -0300

    fix tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,20 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['sans-serif', 'Graphik'],
+				serif: ['Merriweather', 'serif'],
+			},
+			colors: {
+				'indigo': '#360568',
+				'tekhelet': '#5b2a86',
+				'lavender': '#f5e6e8',
+				'thistle': '#D5C6E0',
+				'rose-quarz': '#AAA1C8',
+				'gray': '#8492a6',
+				'gray-light': '#d3dce6',
+			},
+		},
 	},
 	plugins: [],
-	fontFamily: {
-		sans: ['sans-serif', 'Graphik'],
-		serif: ['Merriweather', 'serif'],
-	},
-	colors: {
-		'indigo': '#360568',
-		'tekhelet': '#5b2a86',
-		'lavender': '#f5e6e8',
-		'thistle': '#D5C6E0',
-		'rose-quarz': '#AAA1C8',
-		'gray': '#8492a6',
-		'gray-light': '#d3dce6',
-	},
 }
