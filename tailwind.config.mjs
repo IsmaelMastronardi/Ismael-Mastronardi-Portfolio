c4231b0f1fa2eeea07afca9c69afe0da339dcb9c
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-25 19:07:46 -0300

    add colors and boxShadow to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,9 +15,18 @@ export default {
         'rose-quarz': '#AAA1C8',
         'gray': '#8492a6',
         'gray-light': '#d3dce6',
+        'dark-purple': '#3d194f',
+        'semi-transparent-light-gray': 'rgba(211, 220, 230, 0.4)',
+        'transparent-white': 'rgba(255, 255, 255, 0.5)',
+        'semi-transparent-indigo': 'rgba(54, 5, 104, 0.9)',
+        'extra-transparent-indigo': 'rgba(54, 5, 104, 0.7)',
       },
       spacing: {
         '45%': '45%',
+      },
+      boxShadow: {
+        'xxl': '0 3px 6px rgba(0, 0, 0, 0.26)',
+        '2xxl': '0 6px 12px rgba(0, 0, 0, 0.5)',
       },
     },
   },
