7ef2c9e683ae1a91ee91f343e9067a325271ac16
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-25 19:08:28 -0300

    modify carousel items

--- src/script/carousel.js
+++ src/script/carousel.js
@@ -1,13 +1,13 @@
-import project1 from '../assets/images/AppointWheels.png';
-import project2 from '../assets/images/google.png';
-import project3 from '../assets/images/vscode.png';
-import project4 from '../assets/images/vscode.png';
+import project1 from '../assets/images/project_images/AppointWheels.png'
+import project2 from '../assets/images/project_images/SpaceXploration.png';
+import project3 from '../assets/images/project_images/CbaFestival.png';
+import project4 from '../assets/images/project_images/SpaceXploration.png';
 
 const projects_container = document.getElementById('caroussel')
 
 const projects = [
   {
-    name: 'project1',
+    name: 'AppointWheels',
     img: project1.src,
     live_link: 'p1',
     code_link: 'p1',
@@ -15,27 +15,27 @@ const projects = [
     tech_stack: ['JavaScript', 'React', 'Redux', 'Ruby', 'Ruby on Rails']
   },
   {
-    name: 'project2',
+    name: 'Project2',
     img: project2.src,
     live_link: 'p2',
     code_link: 'p2',
-    description: '',
+    description: 'REACT-AppointWheels is a Full Stack application that allows users to create accounts and rent cars, while allowing admins to create and delete them.',
     tech_stack: ['JavaScript', 'React', 'Redux', 'Ruby', 'Ruby on Rails'],
   },
   {
-    name: 'project3',
+    name: 'Project3',
     img: project3.src,
     live_link: 'p3',
     code_link: 'p3',
-    description: '',
+    description: 'REACT-AppointWheels is a Full Stack application that allows users to create accounts and rent cars, while allowing admins to create and delete them.',
     tech_stack: ['JavaScript', 'React', 'Redux', 'Ruby', 'Ruby on Rails'],
   },
   {
-    name: 'project4',
+    name: 'Project4',
     img: project4.src,
     live_link: 'p4',
     code_link: 'p4',
-    description: '',
+    description: 'REACT-AppointWheels is a Full Stack application that allows users to create accounts and rent cars, while allowing admins to create and delete them.',
     tech_stack: ['Ruby', 'Ruby on Rails'],
   },
 ];
@@ -45,28 +45,33 @@ projects.forEach((el) => {
   item.className = 'w-6/12 flex justify-center items-center absolute right-45% top-1/2 bottom-1/2 carouselItem';
   item.dataset.name = el.name;
 
-  const techStackHTML = el.tech_stack.map(tech => `<p class="bg-indigo">${tech}</p>`).join('');
+  const techStackHTML = el.tech_stack.map(tech => `<li class="bg-dark-purple rounded-md p-1 font-bold shadow-xxl hover:shadow-2xxl cursor-pointer"><p>${tech}</p></li>`).join('');
 
   item.innerHTML = `
-    <div class="absolute bottom-10 z-20 flex flex-row justify-around right-0 left-0 tech-stack" style="display: none;">
-      ${techStackHTML}
+    <div class="absolute z-20 right-0 left-0  top-40 tech-stack" style="display: none;">
+      <ul class="w-full flex flex-row justify-around">
+        ${techStackHTML}
+      </ul>
     </div>
+    <h3 class="text-tekhelet z-20 font-bold hidden">${el.name}</h3>
+    <div class="absolute top-6 bottom-0 right-0 left-0 z-10 rounded-b-lg bg-semi-transparent-light-gray item_hover_bg" style="display: none;"></div>
     <Image src="${el.img}" alt='${el.name}' class="hidden"/>
   `;
 
   item.addEventListener('mouseenter', () => {
     if(!item.firstElementChild.classList.contains('hidden')){
       item.querySelector('.tech-stack').style.display = 'flex';
+      item.querySelector('.item_hover_bg').style.display = 'flex';
     }
   });
 
   item.addEventListener('mouseleave', () => {
     item.querySelector('.tech-stack').style.display = 'none';
+    item.querySelector('.item_hover_bg').style.display = 'none';
   });
 
   projects_container.appendChild(item);
 });
-const title = document.getElementById('project-title')
 
 let current = 0;
 let prev = current - 1 < 0 ? projects.length - 1 : current - 1;
@@ -80,28 +85,33 @@ const projectDescription = document.getElementById('project_description')
 const updateCarousel = () => {
   Array.from(carouseltems).forEach((el, index) => {
     if (index === current) {
-      el.className = 'w-7/12 z-10 relative carouselItem';
+      el.className = 'w-7/12 z-10 relative carouselItem bg-gray-light rounded-xl';
       el.firstElementChild.classList.remove('hidden')
-      el.lastElementChild.className = 'rounded-xl';
+      el.lastElementChild.className = 'rounded-b-xl';
       el.lastElementChild.removeEventListener('click', carouselBack);
       el.lastElementChild.removeEventListener('click', carouselNext);
-      title.innerText = el.dataset.name
+      el.children[1].classList.remove('hidden')
       projectDescription.innerText = projects[current].description
       liveLink.href = projects[current].live_link
       codeLink.href = projects[current].code_link
-    } else if (index === prev) {
+    } 
+    else if (index === prev) {
       el.className = 'w-6/12 flex justify-center items-center absolute right-45% top-1/2 bottom-1/2 carouselItem';
       el.firstElementChild.classList.add('hidden')
+      el.children[1].classList.add('hidden')
       el.lastElementChild.className = 'opacity-60  carouselBack rounded-xl';
       el.lastElementChild.removeEventListener('click', carouselNext);
       el.lastElementChild.addEventListener('click', carouselBack);
-    } else if (index === next) {
+    } 
+    else if (index === next) {
       el.className = 'w-6/12 flex justify-center items-center absolute left-45% top-1/2 bottom-1/2 carouselItem';
       el.firstElementChild.classList.add('hidden')
+      el.children[1].classList.add('hidden')
       el.lastElementChild.className = 'opacity-60 carouselNext rounded-xl';
       el.lastElementChild.removeEventListener('click', carouselBack);
       el.lastElementChild.addEventListener('click', carouselNext);
-    } else {
+    } 
+    else {
       el.className = "w-6/12 flex justify-center items-center absolute left-2/4 top-1/2 bottom-1/2 carouselItem";
       el.firstElementChild.classList.add('hidden')
       el.lastElementChild.className = 'hidden';
@@ -124,12 +134,5 @@ const carouselNext = () => {
   next = current === projects.length - 1 ? 0 : current + 1;
   updateCarousel();
 };
-
-// const prevImg = document.getElementsByClassName("carouselBack");
-// const nextImg = document.getElementsByClassName("carouselNext");
-
-// prevImg[0].addEventListener('click', carouselBack);
-// nextImg[0].addEventListener('click', carouselNext);
-
 
 updateCarousel()
